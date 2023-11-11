eb19530820dfadf6bd448ec0d445a5328ed14d59
Flex <97204274+FlexDynamiteYT@users.noreply.github.com>
2023-11-11 22:34:07 +0000

    vmasomvka

--- pythonCode/app.py
+++ pythonCode/app.py
@@ -20,5 +20,4 @@ def generate_video():
 if __name__ == '__main__':
     # Run the Flask app using Gunicorn from the command line
     # Example command: gunicorn -w 4 -b 0.0.0.0:5000 your_app_module:app
-    import os
-    os.system('gunicorn -w 4 -b 0.0.0.0:5000 app:app')
+    app.run(debug=True)
